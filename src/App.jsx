ca059f3f67162d38cacba57250430c63c944ddb9
vickysehgal <vk1512694@gmail.com>
2023-12-22 09:11:34 +0530

    configured auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 
+import { Routes } from "react-router-dom";
+
+
 function App() {
-  return <>
-    hello world
-  </>;
+  return (
+    <>
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
